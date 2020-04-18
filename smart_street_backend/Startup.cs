522cf6ea81207013979d8265660c50b0dff0e046
kyle <manganyikyle@gmail.com>
2020-04-18 21:42:00 +0200

    cors

--- smart_street_backend/Startup.cs
+++ smart_street_backend/Startup.cs
@@ -51,6 +51,7 @@ namespace smart_street_backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("CorsPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
